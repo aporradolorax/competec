a5c32d9b6a4e3221846a0d62855659ba59b32cbe
Pedro Henrique Chaves de Oliveira <zawardo801@gmail.com>
2025-04-26 11:47:09 -0300

    Update Main.java

--- aula-0/Main.java
+++ aula-0/Main.java
@@ -1,7 +1,15 @@
 public class Main
 {
 	public static void main(String[] args) {
-        // Escreva seu código aqui ⬇️
 		System.out.println("Hello World");
-	}
-}
+
+ double raranja = 2;
+ double nabana = 1;
+ 
+ System.out.println("o preço da banana é R$"+nabana);
+ System.out.println("e o da laranja é R$"+raranja);
+ double soma = raranja+nabana;
+ System.out.println("as duas juntas são R$"+soma);
+ double desconto = soma*0.2;
+ System.out.println("com desconto é R$"+desconto);
+	}}
