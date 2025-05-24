69f43a09e00fc4de224c83aa193c4088b55b6ffb
Pedro Henrique Chaves de Oliveira <zawardo801@gmail.com>
2025-05-24 10:35:35 -0300

    Update Main.java

--- aula-3/Main.java
+++ aula-3/Main.java
@@ -18,3 +18,30 @@ public class Main
     
 	}
 }
+
+
+//att 2
+
+
+import java.util.Scanner;
+public class Main
+{
+    
+	public static void main(String[] args) {
+	Scanner p1 = new Scanner (System.in);
+
+	int num = 1;
+	int soma = 2;
+    
+    while (soma<100){
+        
+    int num2= num + soma;
+    System.out.println(num+ "+"+soma+ "="+num2);
+    num = num2;
+    soma ++ ;
+        
+    }
+
+    
+	}
+}
