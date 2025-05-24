dd520fa93c7bb4ad8f9e7d920a10045490d85901
Pedro Henrique Chaves de Oliveira <zawardo801@gmail.com>
2025-05-24 10:08:59 -0300

    Update Main.java

--- aula-3/Main.java
+++ aula-3/Main.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
 public class Main
 {
+    
 	public static void main(String[] args) {
-        // Escreva seu código aqui ⬇️
-		System.out.println("Hello World");
+	Scanner p1 = new Scanner (System.in);
+	System.out.println("digite uma palavra");
+	String palavra = p1.nextLine();
+	
+	int num = 1;
+    
+    while (num<5){
+    
+    System.out.println(palavra);
+    num ++ ;}
+    
 	}
 }
