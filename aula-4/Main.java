6286036319b14656d37e9755d3670de47e7c2c03
Pedro Henrique Chaves de Oliveira <zawardo801@gmail.com>
2025-05-31 10:36:56 -0300

    Update Main.java

--- aula-4/Main.java
+++ aula-4/Main.java
@@ -1,7 +1,37 @@
+ /////////////////////////////////////////////
+		codes da aula
+    //int !numeros! [] = new int [5];
+    
+    //String !cidades! []= {"nome","outro nome"};
+    
+    //char !caracteres! []= new char [999];
+    
+    //char !alfabetoateF! []= ['a','b','c','d','e','f',]
+    
+    //int tamanho = alfabeto.length;
+    //////////////////////////////////////////////
+
+
+
+//atividade 1
+
 public class Main
 {
 	public static void main(String[] args) {
-        // Escreva seu código aqui ⬇️
-		System.out.println("Hello World");
+    
+    int numeros [] =  {24,69,45};
+ 
+    System.out.println("os numeros da lista são "+numeros [0]+" "+numeros[1]+" "+numeros[2]);
+    System.out.println();
+    int soma= numeros [0]+numeros[1]+numeros[2];
+    System.out.println("se você somalos o resultado será: "+soma);
+
+    
+    
+    
+    
+    
+    
+    
 	}
 }
