a8d889cf0162a0e85457b14af1d78925131e2442
Pedro Henrique Chaves de Oliveira <zawardo801@gmail.com>
2025-06-14 10:15:12 -0300

    Update Main.java

--- aula-5/Main.java
+++ aula-5/Main.java
@@ -1,7 +1,24 @@
+//explicação 1
+
 public class Main
 {
 	public static void main(String[] args) {
-        // Escreva seu código aqui ⬇️
-		System.out.println("Hello World");
+		
+	//	int lin = 3;
+	// int tabela [][] = new int [lin] [2];
+	//	tabela [0][0]=0;
+	//	tabela [0][1]=1;
+	//	tabela [1][0]=2;
+	//	tabela [1][1]=3;
+	//	tabela [2][0]=4;
+	//	tabela [2][1]=5;
+		
+	//	int[][] tabela = {{0,1},
+	//	                  {2,3},
+	//	                  {4,5}};
+        
+		
+		
 	}
 }
+
