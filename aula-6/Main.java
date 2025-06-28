68965882c5c82fb234dad170f4343b9d9789e8f4
Pedro Henrique Chaves de Oliveira <zawardo801@gmail.com>
2025-06-28 10:35:57 -0300

    Update Main.java

--- aula-6/Main.java
+++ aula-6/Main.java
@@ -10,3 +10,31 @@ public class Main
 		System.out.println(nome+" tem "+tamanho+" letras");
 	}
 }
+
+
+//ATT 2
+
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner p1 = new Scanner(System.in);
+		String senha = "batata";
+		String tent = p1.nextLine();
+		
+		if ( tent.equalsIgnoreCase(senha)){
+		    System.out.println("parabens você acertou!!");
+		   
+		}
+		else{
+		    System.out.println("senha incorreta");
+		}
+		
+		
+		
+		
+	}
+}
+
+
+
